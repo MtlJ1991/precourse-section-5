46d283c6688a1e121aa741f9116b05181f921c7b
MtlJ1991 <MtlJ1991@gmail.com>
2017-11-02 17:22:36 +0000

    Updated CSS, score more visible. Tweaks to make JS easier to read. Range = Score.

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 function check () {
-let question1 = document.theQuiz.question1.value;
-let question2 = document.theQuiz.question2.value;
-let question3 = document.theQuiz.question3.value;
-let correct = 0;
-
-if(question1 === "Frieza"){
-    correct++;
-}
-if(question2 === "Hour"){
-    correct++;
-}
-if(question3 === "Hat"){
-    correct++;
-}
-// Message will be printed
-
-let response = ["You've acsended!", "You're powering up, almost there!", "Trip to the time chamber required!", "Krillen puts you to shame!"];
-let pictures = ["gifs/vegeto.gif", "gifs/vegeta.gif", "gifs/time.gif", "gifs/krillin.gif"];
-let range;
-
-if(correct <= 0 ){
-    range = 3;
-}
-if(correct > 0 && correct < 2 ){
-    range = 2;
-}
-if(correct > 1  && correct < 3){
-    range= 1;
-}
-if(correct  > 2){
-    range= 0;
-}
-document.getElementById("submitted").style.visibility = "visible";
-document.getElementById("response").innerHTML = response[range];
-document.getElementById("number_correct").innerHTML = "You scored " + correct + " correct answers!" ;
-document.getElementById("picture").src = pictures[range];
-
-}
+    let question1 = document.theQuiz.question1.value;
+    let question2 = document.theQuiz.question2.value;
+    let question3 = document.theQuiz.question3.value;
+    let correct = 0;
+    
+    if(question1 === "Frieza"){
+        correct++;
+    }
+    if(question2 === "Hour"){
+        correct++;
+    }
+    if(question3 === "Hat"){
+        correct++;
+    }
+    // Message will be printed
+    
+    let response = ["You've acsended!", "You're powering up, almost there!", "Trip to the time chamber required!", "Krillen puts you to shame!"];
+    let pictures = ["gifs/vegeto.gif", "gifs/vegeta.gif", "gifs/time.gif", "gifs/krillin.gif"];
+    let score;
+    
+    if(correct <= 0 ){
+        score = 3;
+    }
+    if(correct > 0 && correct < 2 ){
+        score = 2;
+    }
+    if(correct > 1  && correct < 3){
+        score= 1;
+    }
+    if(correct  > 2){
+        score= 0;
+    }
+    document.getElementById("submitted").style.visibility = "visible";
+    document.getElementById("response").innerHTML = response[score];
+    document.getElementById("number_correct").innerHTML = "Your current score is " + correct + " !";
+    document.getElementById("picture").src = pictures[score];
+    
+    }
